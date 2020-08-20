6a600d33a31e2238f8af4782e86abe980c5cf732
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 18:11:55 +0800

    [测试景深] add split related test cases.

--- src/test/java/org/katas/refactoring/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/org/katas/refactoring/wordfrequency/WordFrequencyGameTest.java
@@ -1,5 +1,6 @@
 package org.katas.refactoring.wordfrequency;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -12,4 +13,38 @@ public class WordFrequencyGameTest {
         String result = wordFrequencyGame.getResult("the the is");
         assertThat(result, is("the 2\nis 1"));
     }
+
+    // split start
+
+    @Test
+    public void should_split_input_by_1_space() {
+        WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
+        String result = wordFrequencyGame.getResult("the the");
+        assertThat(result, is("the 2"));
+    }
+
+    @Test
+    public void should_split_input_by_n_space() {
+        WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
+        String result = wordFrequencyGame.getResult("the is the is");
+        assertThat(result, is("the 2\nis 2"));
+    }
+
+    @Test
+    public void should_split_input_to_one_element_given_one_word() {
+        WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
+        String result = wordFrequencyGame.getResult("the");
+        assertThat(result, is("the 1"));
+    }
+
+    //TODO found a bug
+    @Ignore
+    @Test
+    public void should_space_will_not_be_counted() {
+        WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
+        String result = wordFrequencyGame.getResult("the      ");
+        assertThat(result, is("the 1"));
+    }
+
+    // split end
 }
