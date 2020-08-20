5730321c35f5e29450db050913aa03e964bed948
Songbai Yan <colorguitar@hotmail.com>
2020-08-20 17:41:14 +0800

    [搬移功能测试][冒烟测试] add smoke test.

--- src/test/java/org/katas/refactoring/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/org/katas/refactoring/wordfrequency/WordFrequencyGameTest.java
@@ -1,15 +1,15 @@
 package org.katas.refactoring.wordfrequency;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.katas.refactoring.wordfrequency.WordFrequencyGame;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-
 public class WordFrequencyGameTest {
-
-
+    @Test
+    public void smoke_test() {
+        WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
+        String result = wordFrequencyGame.getResult("the the is");
+        assertThat(result, is("the 2\nis 1"));
+    }
 }
