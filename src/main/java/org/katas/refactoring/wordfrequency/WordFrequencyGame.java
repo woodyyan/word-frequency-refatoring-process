128ee87f9d76ed9766b52a2399b9ca72ab68fa78
Songbai Yan <colorguitar@hotmail.com>
2020-08-21 11:27:33 +0800

    [旧的不变] extract methods.

--- src/main/java/org/katas/refactoring/wordfrequency/WordFrequencyGame.java
+++ src/main/java/org/katas/refactoring/wordfrequency/WordFrequencyGame.java
@@ -1,47 +1,58 @@
 package org.katas.refactoring.wordfrequency;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
+        try {
+            List<Input> inputList = transferToDomainModel(inputStr);
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
-        } else {
+            inputList = countWord(inputList);
 
-            try {
+            inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
+            return renderResult(inputList);
+        } catch (Exception e) {
+            return "Calculate Error";
         }
+    }
+
+    private List<Input> transferToDomainModel(String inputStr) {
+        //split the input string with 1 to n pieces of spaces
+        String[] arr = inputStr.split("\\s+");
+
+        List<Input> inputList = new ArrayList<>();
+        for (String s : arr) {
+            Input input = new Input(s, 1);
+            inputList.add(input);
+        }
+        return inputList;
+    }
+
+    private List<Input> countWord(List<Input> inputList) {
+        //get the map for the next step of sizing the same word
+        Map<String, List<Input>> map = getListMap(inputList);
+
+        List<Input> list = new ArrayList<>();
+        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue().size());
+            list.add(input);
+        }
+        inputList = list;
+        return inputList;
+    }
+
+    private String renderResult(List<Input> inputList) {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Input w : inputList) {
+            String s = w.getValue() + " " + w.getWordCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
     }
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
@@ -60,7 +71,7 @@ public class WordFrequencyGame {
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         WordFrequencyGame game = new WordFrequencyGame();
 
         String result = game.getResult("the the is");
